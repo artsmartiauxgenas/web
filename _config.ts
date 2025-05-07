78b691982de881381d985f3838d44c59f30a5b9b
geoffreypetri <g.petri@me.com>
2025-05-07 21:08:18 +0200

    Connect form

--- _config.ts
+++ _config.ts
@@ -3,5 +3,6 @@ import lume from "lume/mod.ts";
 const site = lume();
 site.copy("_includes/css/style.css");
 site.copy("_includes/images/logo.png");
+site.data("form_endpoint", Deno.env.get("FORM_ENDPOINT"));
 
 export default site;
