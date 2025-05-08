67d2929f8bf735596f6dd8f3b2e15d89bb880b10
Geoffrey PETRI <g.petri@me.com>
2025-05-08 15:05:02 +0200

    Update _config.ts

--- _config.ts
+++ _config.ts
@@ -4,6 +4,7 @@ const site = lume();
 site.copy("_includes/css/style.css");
 site.copy("_includes/images/logo_dragon.png");
 site.copy("_includes/images/logo_tigre.png");
+//site.copy("static");
 site.data("form_endpoint", Deno.env.get("FORM_ENDPOINT"));
 
 export default site;
