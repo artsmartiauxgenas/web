d67c9334af76944810d4f87da2ca604f58169613
geoffreypetri <g.petri@me.com>
2025-05-08 15:25:15 +0200

    Remove secret for form endpoint

--- _config.ts
+++ _config.ts
@@ -5,6 +5,5 @@ site.copy("_includes/css/style.css");
 site.copy("_includes/images/logo_dragon.png");
 site.copy("_includes/images/logo_tigre.png");
 //site.copy("static");
-site.data("form_endpoint", Deno.env.get("FORM_ENDPOINT"));
 
 export default site;
