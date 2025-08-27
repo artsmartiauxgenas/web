cfb97c5b288b8ee7326f77440af63413db057462
geoffreypetri <g.petri@me.com>
2025-08-27 18:07:06 +0200

    Add members.data

---
+++ data/members.data.ts
@@ -0,0 +1,30 @@
+const members = {
+  '2024-25': [11, 24, 73, 79, 80, 82, 83, 83, 83, 83, 83, 83],
+  '2025-26': [12, 31]
+}
+
+export default {
+  load() {
+    return members
+  }
+}
+
+// [
+//   {
+//     'date': "2025-08-18",
+//     'section': "karaté-6-8",
+//     'sex': "M",
+//     'age': 43,
+//     'place': "Genas",
+//     'subscription': 240
+//   },
+//   {
+//     'date': "2025-08-18",
+//     'section': "karaté-6-8",
+//     'sex': "M",
+//     'age': 43,
+//     'place': "Genas",
+//     'subscription': 240
+//   },
+
+// ]
