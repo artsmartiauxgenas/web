ac3bfe104f58786d997f58a20a7fb66de432c956
geoffreypetri <g.petri@me.com>
2025-08-27 13:03:48 +0200

    Add chart Membres

---
+++ .vitepress/theme/components/Chart.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+  <div ref="chartRef" class="chart"></div>
+</template>
+
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import * as echarts from 'echarts'
+
+const props = defineProps({
+  option: {
+    type: Object,
+    required: true,
+  },
+})
+
+const chartRef = ref(null)
+let chartInstance = null
+
+onMounted(() => {
+  if (chartRef.value) {
+    chartInstance = echarts.init(chartRef.value)
+    chartInstance.setOption(props.option)
+  }
+})
+
+onBeforeUnmount(() => {
+  if (chartInstance) {
+    chartInstance.dispose()
+  }
+})
+</script>
+
+<style scoped>
+.chart {
+  width: 100%;
+  height: 400px;
+}
+</style>
